0b011cef29f25fde0178fc3bacfc121a1d067256
JackMarx <seriousfools@gmail.com>
2019-03-05 09:02:15 -0600

    initial commit

---
+++ db/migrate/20190304164941_create_tasks.rb
@@ -0,0 +1,14 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.text :description
+      t.integer :priority
+      t.datetime :deadline
+      t.boolean :complete
+      t.integer :duration
+      t.boolean :favorite
+
+      t.timestamps
+    end
+  end
+end
