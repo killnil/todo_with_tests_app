db1d4d1408403173e2076566d23171807d34ea1a
JackMarx <seriousfools@gmail.com>
2019-03-05 09:38:34 -0600

    task tests

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -14,4 +14,92 @@ RSpec.describe Task, type: :model do
       expect(task.complete).to eq(true)
     end
   end
+
+  describe '#toggle_favorite!' do 
+    it 'should switch favorite to false if it began as true' do
+      task = Task.create(favorite: true)
+      task.toggle_favorite!
+      expect(task.favorite).to eq(false)
+    end
+
+    it 'should switch favorite to true if it began as false' do
+      task = Task.create(favorite: false)
+      task.toggle_favorite!
+      expect(task.favorite).to eq(true)
+    end
+  end
+
+  describe '#overdue?' do 
+    it 'should return true if deadline of the task is earlier than now and complete is false' do 
+      task = Task.create(deadline: 1.hour.ago, complete: false)
+      expect(task.overdue?).to eq(true)
+    end
+
+    it 'should return false if deadline of the task is earlier than now and complete is true' do 
+      task = Task.create(deadline: 1.hour.ago, complete: true)
+      expect(task.overdue?).to eq(false)
+    end
+
+    it 'should return false if deadline of the task is later than now and complete is false' do 
+      task = Task.create(deadline: 1.hour.from_now, complete: false)
+      expect(task.overdue?).to eq(false)
+    end
+  end
+
+  describe '#increment_priority!' do 
+    it 'should increase priority by 1' do 
+      task = Task.create(priority: 5)
+      task.increment_priority!
+      expect(task.priority).to eq(6)
+    end
+
+    it 'should not increase the priority beyond 10' do 
+      task = Task.create(priority: 10)
+      task.increment_priority!
+      expect(task.priority).to eq(10)
+    end
+  end
+
+  describe '#decrement_priority!' do 
+    it 'should decrease priority by 1' do 
+      task = Task.create(priority: 5)
+      task.decrement_priority!
+      expect(task.priority).to eq(4)
+    end
+
+    it 'should not decrease the priority beyond 1' do 
+      task = Task.create(priority: 1)
+      task.decrement_priority!
+      expect(task.priority).to eq(1)
+    end
+  end
+
+  describe '#snooze_hour!' do 
+    it 'should delay the deadline by 1 hour' do 
+      time_stamp = Time.now
+      task = Task.create(deadline: time_stamp)
+      task.snooze_hour!
+      expect(task.deadline).to eq(time_stamp + 1.hour)
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
